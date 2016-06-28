42ee309f588989ff5b43ca32b006b06e6329ae68
Vicente Canales <me@devjunk.org>
2016-06-28 11:38:17 -0400

    Add transpiled index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+export { default as addBig } from './components/Buttons/addBig';
+export { default as Calendar } from './components/Calendar/Calendar';
+export { default as Cropper } from './components/Cropper/Cropper';
+export {
+    Form,
+    Input,
+    Select,
+    Fields,
+    ColorPicker,
+    Textarea,
+    Checkbox,
+    File,
+    List,
+    FileMeta,
+    RichEditor
+} from './components/Form/Form';
+export { Grid, Column, Row } from './components/Grid/Grid';
+export { default as Modal } from './components/Modal/Modal';
+export { default as Playlist } from './components/Playlist/Playlist';
+export { default as Separator } from './components/Separator/Separator';
+export { default as Table } from './components/Table/Table';
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+
+var definePlugin = new webpack.DefinePlugin({
+    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+    __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
+});
+
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+
+module.exports = {
+    cache: true,
+    entry: {
+        main:  './index.js'
+    },
+    output: {
+        path: './build',
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(jsx|js)?$/,
+                exclude: /node_modules/,
+                loader: 'react-hot!babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            },
+            {
+                test: /[\/\\]node_modules[\/\\]some-module[\/\\]index\.js$/,
+                loader: 'imports?this=>window'
+            },
+            {
+                test: /vendor\/.+\.(jsx|js)$/,
+                loader: 'imports?jQuery=jquery,$=jquery,this=>window'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js','.jsx']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        definePlugin,
+        commonsPlugin
+    ]
+};
