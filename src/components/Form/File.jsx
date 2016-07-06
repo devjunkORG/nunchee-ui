1611ec948be4c843dfce5d3357bccd6c86e672ac
Vicente Canales <me@devjunk.org>
2016-07-06 17:39:48 -0400

    Update File component to allow file upload by itself (it used to be just a button). Add option to allow multiple files or limit to just one. Show thumbnail of file being added

--- src/components/Form/File.jsx
+++ src/components/Form/File.jsx
@@ -1,50 +1,110 @@
 import React from 'react';
-
+import _ from 'lodash';
 import AddButton from '../Buttons/addBig';
 
-class Field extends React.Component {
+class File extends React.Component {
 
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
+        this.processUpload = this.processUpload.bind(this);
+        this.addFile = this.addFile.bind(this);
+    }
+
+    initialize() {
+        return {
+            files: []
+        };
+    }
+
+    componentWillMount() {
+        this.state = this.initialize();
     }
 
     handleClick(e) {
         e.preventDefault();
         if (this.props.onClick) {
-            this.props.onClick();
+            return this.props.onClick();
         }
+        // this._fileInput.click();
+    }
+
+    processUpload(e) {
+        if (e.target.files && e.target.files.length > 0) {
+            this.addFile(e.target.files[0]);
+        }
+    }
+
+    addFile(file) {
+        let reader = new FileReader();
+        reader.onload = event => {
+            let files = this.state.files;
+            let file = {
+                name: event.target.name,
+                size: event.target.size,
+                url: `url(${event.target.result})`
+            };
+            if (this.props.multiple) {
+                files.push(file);
+                return this.setState({ files: files });
+            }
+            return this.setState({ files: [ file ]});
+        };
+        reader.readAsDataURL(file);
     }
 
     render() {
         let containerStyle = {
-            display: 'flex'
+            display: 'flex',
+            position: 'relative'
         };
         let addButtonStyle = {
             borderRadius: '4px'
         };
-        let itemStyle = {
-            height: '80px',
-            width: '80px',
-            backgroundColor: 'rgba(0,0,0,0.8)',
-            marginRight: '5px',
-            borderRadius: '4px'
+
+        let fileInputStyle = {
+            position: 'absolute',
+            opacity: 0,
+            top: 0,
+            bottom: 0,
+            height: 80,
+            width: 80,
+            cursor: 'pointer'
         };
         return (
             <div className="field">
                 {this.props.label ? <label>{ this.props.label }</label> : ''}
                 <div style={ containerStyle }>
                     <AddButton
+                        ref={ button => this._addButton = button }
                         onClick={ this.handleClick }
                         style={ addButtonStyle }
+                    >
+                    </AddButton>
+                    <input
+                        ref={ input => this._fileInput = input }
+                        type="file"
+                        name="tempFile"
+                        style={ fileInputStyle }
+                        onChange={ this.processUpload }
                     />
-                    <div style={ itemStyle }>
+                    {this.state.files.map((item,key) => {
+                        let itemStyle = {
+                            height: '80px',
+                            width: '80px',
+                            backgroundColor: 'rgba(0,0,0,0.8)',
+                            marginRight: '5px',
+                            borderRadius: '4px',
+                            backgroundImage: item.url
+                        };
+                        return <div key={ key } style={ itemStyle }>
 
-                    </div>
+                        </div>;
+                    })}
                 </div>
             </div>
         );
     }
 }
 
-export default Field;
+export default File;
