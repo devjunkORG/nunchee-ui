4feb2220507e481716811ec09f7542ae080b2b03
Vicente Canales <me@devjunk.org>
2016-08-29 15:46:04 -0300

    Fix config include path

---
+++ src/config.js
@@ -0,0 +1,14 @@
+module.exports =
+{
+    media: {
+        imageSizes: [ 1, 0.75, 0.5, 0.357, 0.25 ],
+        imageTypes: {
+            backdrop: '1600x900',
+            mediumv: '800x600',
+            mediumh: '600x800',
+            poster: '600x900',
+            banner: '1600x320',
+            square: '600x600'
+        }
+    }
+};
